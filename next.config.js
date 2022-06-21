6400e0a22b1e0e40eede8267b05d84493d6c2f8b
jangkyoungtae <zxs3710@naver.com>
2022-06-21 10:05:17 +0900

    next 연습

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
+const API_KEY = process.env.API_KEY;
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	async redirects() {
+		return [
+			{
+				source: "/old/:path*",
+				destination: "/new/:path*",
+				permanent: false,
+			},
+		];
+	},
+	async rewrites() {
+		return [
+			{
+				source: "/api/movies",
+				destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-Ko&page=1`,
+			},
+		];
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from "next/app";
+import React from "react";
+import Layout from "../components/Layour";
+import NavBar from "../components/NavBar";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: AppProps) {
+	return (
+		<>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</>
+	);
 }
-
-export default MyApp
